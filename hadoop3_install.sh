4c5682faaf294e6e42d79f8bea0930bb4d78507e
Sandeep Kumar <sandeeprhct@gmail.com>
2018-01-15 18:33:08 +0200

    Update hadoop3_install.sh

--- hadoop3_install.sh
+++ hadoop3_install.sh
@@ -7,18 +7,13 @@ group='hadoop'
 password='hadoop'
 tmpdir='/tmp/apache_hadoop'
 usr_dir='/usr/local/apache_hadoop'
+git_dir='/home/hadoop/gitrepo'
 gitrepo_li='https://github.com/sandeepksaini/linuxserversetup'
 gitrepo_hadoop='https://github.com/sandeepksaini/HDP-HadoopCluster'
 hadoop_package='http://apache.is.co.za/hadoop/common/hadoop-3.0.0/hadoop-3.0.0.tar.gz'
 
 #Basic package installation
 yum install git sshpass ssh wget -y
-
-#Cloning repo for script utilization
-for list in ${gitrepo_li} ${gitrepo_hadoop}
-do
-  git clone ${list}
-done
 
 # Create user and group
 useradd -a ${user}
@@ -27,11 +22,18 @@ echo "hadoop"|passwd --stdin ${password}
 #Temporary dir creation and applying permission of hadoop user and group
 mkdir -p ${tmpdir}
 mkdir -p ${usr_dir}
+mkdir -p ${git_dir}
 wget -P ${tmpdir} ${hadoop_package} 
 tar -xvf ${tmpdir}/*.gz -C ${tmpdir}
 rm -rf ${tmpdir}/*.gz
 chown -R ${user}:${group} ${tmpdir}
 mv ${tmpdir}/hadoop* ${usr_dir}
+
+#Cloning repo for script utilization
+for list in ${gitrepo_li} ${gitrepo_hadoop}
+do
+  git clone ${list}
+done
 
 # SSH Key generation and copy for seemless ssh and allows hadoop to run without issues
 su ${user} -c 'ssh-keygen -t RSA -P "" -f ~/.ssh/id_rsa'
