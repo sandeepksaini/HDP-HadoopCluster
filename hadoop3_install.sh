e4684791602c9800367e5dde8fbaa4e72becfb71
Sandeep Kumar <sandeeprhct@gmail.com>
2018-02-09 13:43:59 +0200

    Update hadoop3_install.sh

--- hadoop3_install.sh
+++ hadoop3_install.sh
@@ -10,10 +10,10 @@ usr_dir='/usr/local/apache_hadoop'
 git_dir='/home/hadoop/gitrepo'
 gitrepo_linux='https://github.com/sandeepksaini/linuxserversetup'
 gitrepo_hadoop='https://github.com/sandeepksaini/HDP-HadoopCluster'
-hadoop_package='http://apache.is.co.za/hadoop/common/hadoop-3.0.0/hadoop-3.0.0.tar.gz'
+hadoop_pack_link='http://apache.is.co.za/hadoop/common/hadoop-3.0.0/hadoop-3.0.0.tar.gz'
 
 #Basic package installation
-yum -q install git sshpass ssh wget -y
+yum -q install git sshpass ssh wget curl -y
 
 # Create user and group
 useradd ${user}
@@ -23,7 +23,7 @@ echo "hadoop"|passwd --stdin ${password}
 mkdir -p ${tmpdir}
 mkdir -p ${usr_dir}
 mkdir -p ${git_dir}
-wget -q -P ${tmpdir} ${hadoop_package}
+wget -q -P ${tmpdir} ${hadoop_pack_link}
 tar -xf ${tmpdir}/*.gz -C ${tmpdir}
 rm -rf ${tmpdir}/*.gz
 chown -R ${user}:${group} ${tmpdir}
@@ -40,7 +40,7 @@ do
   cd -
 done
 
-# SSH Key generation and copy for seemless ssh and allows hadoop to run without issues
+# SSH Key generation and copy - Provides seemless access to server using ssh for hadoop user-id
 su ${user} -c 'ssh-keygen -t RSA -P "" -f ~/.ssh/id_rsa'
 su ${user} -c 'user='hadoop';password='hadoop';sshpass -p ${password} ssh-copy-id -i /home/${user}/.ssh/id_rsa.pub -o StrictHostKeyChecking=no ${user}@`hostname`'
 systemctl restart sshd
